bd73877c7bd5fdf5770aecb215541537c139723f
atinybeardedman <sedickinson9123@gmail.com>
2014-07-07 10:15:25 -0400

    Update namegrabber.js

--- namegrabber.js
+++ namegrabber.js
@@ -1,6 +1,8 @@
 (function($){
 var as = $('.ex_product_name>a');
+var names=new Array();
 for (var i = 0; i<as.length;i++){
-console.log(as[i].text());	
+names.push(as[i].text);	
 };
+alert(names);
 })(jQuery)
