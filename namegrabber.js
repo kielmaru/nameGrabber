8484a53cf00c6ea44a8249ec47350dd97edfc10a
atinybeardedman <sedickinson9123@gmail.com>
2014-07-07 10:04:08 -0400

    Update namegrabber.js

--- namegrabber.js
+++ namegrabber.js
@@ -1,5 +1,5 @@
 (function($){
-var as = $('.ex_product_name>a').each(function(){this.innerHTML});
+var as = $('.ex_product_name>a').each(function(){this.text()});
 for (var i = 0; i<as.length;i++){
 console.log(as[i]);	
 };
